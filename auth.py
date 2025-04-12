834416b2b4f9901f4dfaca191d59c7d5b7873344
jwn7 <ksyzing@naver.com>
2025-04-12 16:14:03 +0900

    수정중

--- auth.py
+++ auth.py
@@ -1,32 +1,15 @@
-import sqlite3
-from flask import Blueprint, render_template, request, redirect, url_for, session, g, flash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User  # User 모델을 가져오기 위해 수정
 
 bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates/auth')
 
-DATABASE = 'user.db'  # app.py에서 관리하거나 설정 파일로 분리하는 것이 좋음
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
-
+# 세션에서 사용자 정보를 가져오는 함수
 def get_user():
     user_id = session.get('user_id')
     if user_id is None:
         return None
-    db = get_db()
-    return db.execute(
-        'SELECT * FROM user WHERE id = ?', (user_id,)
-    ).fetchone()
-
-@bp.teardown_request
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+    return User.query.get(user_id)  # Flask-SQLAlchemy 방식으로 수정
 
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -34,28 +17,24 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         error = None
-        db = get_db()
 
+        # 아이디와 비밀번호 검증
         if not username:
             error = '아이디를 입력해주세요.'
         elif not password:
             error = '비밀번호를 입력해주세요.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif User.query.filter_by(username=username).first() is not None:
             error = f'{username}은 이미 존재하는 아이디입니다.'
 
         if error is None:
             hashed_password = generate_password_hash(password)
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, hashed_password)
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))  # 블루프린트 이름 명시
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))  # 로그인 페이지로 리다이렉트
 
-        flash(error)
-    return render_template('signup.html')  # templates/auth/signup.html
+        flash(error)  # 에러 메시지 출력
+    return render_template('signup.html')  # 템플릿 경로 수정
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -63,24 +42,22 @@ def login():
         username = request.form['username']
         password = request.form['password']
         error = None
-        db = get_db()
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+
+        user = User.query.filter_by(username=username).first()
 
         if user is None:
             error = '존재하지 않는 아이디입니다.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = '비밀번호가 일치하지 않습니다.'
 
         if error is None:
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))  # 메인 앱의 엔드포인트
+            session['user_id'] = user.id  # 세션에 사용자 ID 저장
+            return redirect(url_for('index'))  # 메인 페이지로 리다이렉트
 
-        flash(error)
-    return render_template('login.html')    # templates/auth/login.html
+        flash(error)  # 에러 메시지 출력
+    return render_template('login.html')  # 템플릿 경로 수정
 
 @bp.route('/logout')
 def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('index'))      # 메인 앱의 엔드포인트
+    session.pop('user_id', None)  # 세션에서 user_id를 제거하여 로그아웃 처리
+    return redirect(url_for('index'))  # 메인 페이지로 리다이렉트
--- models.py
+++ models.py
@@ -2,10 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+# 게시판 모델 정의
+class BulletinBoard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+# 댓글 모델 정의
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    post_id = db.Column(db.Integer, nullable=False)  # 게시글 ID
+    post_id = db.Column(db.Integer, db.ForeignKey('bulletin_board.id'), nullable=False)  # 게시글 ID
     parent_id = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=True)  # 대댓글의 부모 ID
 
     # 대댓글 관계 설정
